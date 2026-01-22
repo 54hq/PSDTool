f2917b791a7bc8b13ed2f87dd00eb7abdd5cca73
Li <84608852@qq.com>
2026-01-22 19:45:16 +0800

    移除多余功能

--- jsx/main.jsx
+++ jsx/main.jsx
@@ -7,48 +7,6 @@ var standardizeLayerNames = function() {
     }
 }
 
-var renameLayersWithLayerId = function() {
-    try {
-        alert("开始编号，中途请不要操作");
-        RenameLayers();
-        alert("编号完成");
-    } catch (e) {
-        alert("编号失败，请规范图层命名\n" + e);
-    }
-}
-
-var createLayoutList = function(listType, layoutType, maxColumn, maxRow, gapHorizontal, gapVertical, paddingTop, paddingLeft) {
-    try {
-        alert("开始生成结构，中途请不要操作");
-        var layoutInfo = new LayoutInfo(layoutType, maxColumn, maxRow, gapHorizontal, gapVertical, paddingTop, paddingLeft)
-        CreateLayoutList(listType, layoutInfo);
-        alert("创建完成");
-    } catch (e) {
-        alert(e)
-    }
-}
-
-var previewListLayout = function(layoutType, maxColumn, maxRow, gapHorizontal, gapVertical, paddingTop, paddingLeft) {
-    try {
-        alert("开始生成预览，中途请不要操作");
-        var layoutInfo = new LayoutInfo(layoutType, maxColumn, maxRow, gapHorizontal, gapVertical, paddingTop, paddingLeft)
-        PreviewListLayout(layoutInfo);
-        alert("生成预览完成");
-    } catch (e) {
-        alert(e);
-    }
-}
-
-var hideIgnoreLayer = function() {
-    try {
-        alert("开始隐藏图层，中途请不要操作");
-        HideIgnoreLayer(app.activeDocument.layers);
-        alert("隐藏完成");
-    } catch (e) {
-        alert(e);
-    }
-}
-
 var checkFont = function() {
     try {
         alert("开始检查字体，中途请不要操作");
@@ -105,85 +63,8 @@ var checkSameNameImage = function() {
     }
 }
 
-var ignoreSelectLayerButton = function() {
-    IgnoreSelectLayer();
-}
-
-var comparePsds = function() {
-    alert("对比打开的两个PSD文件")
-    ComparePsds();
-}
-
-var printSelectLayerSize = function() {
-    PrintSelectLayerSize();
-}
-
-var CreateTempPurePSD = function () {
-    if (app.documents.length <= 0) {
-        return;
-    }
-    try {
-        var originalDoc = app.activeDocument;
-        var duplicatedDoc = originalDoc.duplicate();
-        DeleteAllIgnoreLayer(duplicatedDoc);
-        var tempFile = new File(Folder.temp + "/temp_pure_psd.psd");
-        var saveOptions = new PhotoshopSaveOptions();
-        saveOptions.layers = true;
-        duplicatedDoc.saveAs(tempFile, saveOptions, true);
-        duplicatedDoc.close(SaveOptions.DONOTSAVECHANGES);
-        app.open(tempFile);
-        alert("纯净PSD临时文件已生成：" + Folder.temp + "/temp_pure_psd.psd");
-    }
-    catch (e) {
-        alert(e);
-    }
-}
-
-var createScrollViewMask = function(){
-    var activeLayer = app.activeDocument.activeLayer;
-    var isSpecialLayoutGroup = IsSpecialLayoutLayer(activeLayer.name);
-    if (!isSpecialLayoutGroup) {
-        alert("没有选中ScrollView");
-        return;
-    }
-    BuildSelectionBaseOnLayer(activeLayer)
-    CreateLayerMask()
-    BuildSelectionBaseOnLayer(activeLayer)
-}
-
-var genSpecifiedImage = function(){
-    GenSpecifiedImage();
-}
-
-var previewFormat = function(){
-    PreviewFormat();
-}
-
-var getLayerCount = function(){
-    GetLayerCount();
-}
-
-var showAllLayer = function(){
-    ShowAllLayer();
-}
-
 var checkSmartObjectFilterFX = function(){
     CheckSmartObjectFilterFX();
-}
-
-var checkGroupEffect = function(){
-    CheckGroupEffect();
-}
-
-var gammaCorrection = function(value){
-    alert("修正");
-    try{
-        GammaCorrection(value);
-    }
-    catch(e){
-        alert(e);
-    }
-    alert("结束");
 }
 
 var deleteEmptyLayer = function(){
@@ -196,38 +77,5 @@ var deleteEmptyLayer = function(){
         alert(report);
     } else {
         alert("没有空白图层");
-    }
-}
-
-var amendMode = function(){
-    alert("开始修正")
-    AmendMode();
-    alert("结束修正")
-}
-
-var removePreviewLayout = function(){
-    RemovePreviewLayout();
-    alert("移除完成");
-}
-
-var checkSameLayerId = function(){
-    CheckSameLayerId();
-}
-
-var clearLock = function(){
-    ClearLock();
-    alert("清除完成")
-}
-
-var createIconSize = function(width, height){
-    CreateIconSize(width, height);
-}
-
-var checkTextEffect = function(){
-    try{
-        CheckTextEffect();
-    }
-    catch(e){
-        alert(e);
     }
 }
